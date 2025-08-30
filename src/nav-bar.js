54a266404afa933bfbc852dccab70f3b2ad1abb5
Coldobird <twuestefeld@gmail.com>
2025-08-30 00:09:17 -0300

    wip

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -1,23 +1,65 @@
-import React from "react";
+import githubIcon from './midia/githubIcon.svg';
+import linkedInIcon from './midia/linkedInIcon.svg';
 
-function TitleText() {
+function TitleTextName() {
   return (
     <pre>
-{String.raw`
-  _____ _                                
- |_   _| |__   ___  _ __ ___    __ _ ___ 
-   | | | '_ \ / _ \| '_ ◝ _ \ / _◝ / __|
-   | | | | | | (_) | | | | | | (_|  \__ \
-   |_| |_| |_|\___/|_| |_| |_|\__,_|____/ 
+      {String.raw`
+ _____ _                                
+|_   _| |__   ___  _ __ ___    __ _ ___ 
+  | | | '_ \ / _ \| '_ ' _ \ / _ ' / __|
+  | | | | | | (_) | | | | | | (_|  \__ \
+  |_| |_| |_|\___/|_| |_| |_|\__,_|____/
 `}
     </pre>
   );
 }
 
+function TitleTextSurname() {
+  return (
+    <pre className='surname'>
+      {String.raw`
+__        __               _        __      _     _ 
+\ \      / /   _  ___  ___| |_ ___ / _| ___| | __| |
+ \ \ /\ / / | | |/ _ \/ __| __/ _ \ |_ / _ \ |/ _  |
+  \ V  V /| |_| |  __/\__ \ ||  __/  _|  __/ | (_| |
+   \_/\_/  \__,_|\___||___/\__\___|_|  \___|_|\__,_|
+`}
+    </pre>
+  );
+}
+
+function TitleTextTitle() {
+  return (
+    <pre className='title'>
+      {String.raw`
+____  __.__  __  __ ____  .___. _.  _. .___
+||== ((   )) ||\\||  ||   ||==  ||\\|| ||  )
+||    \\_//  || \||  ||   ||__. || \|| ||_/                                                  
+`}
+    </pre>
+  );
+}
+
+function MyInfo() {
+  return (
+  <my-info>
+    <p className="location">Curitiba,brazil</p>
+    <social-buttons>
+      <img className="social-button" src={githubIcon}></img>
+      <img className="social-button" src={linkedInIcon}></img>
+    </social-buttons>
+  </my-info>
+)
+}
+
 function NavBar() {
   return (
     <nav-bar>
-      <TitleText />
+      <TitleTextName />
+      <TitleTextSurname />
+      <TitleTextTitle />
+      <MyInfo />
     </nav-bar>
   );
 }
